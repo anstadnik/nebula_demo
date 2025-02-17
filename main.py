8321ed22c121fcc271c62b9dd2b6d848395a5ac3
Galois <andrii.s@openmindsinstitute.org>
2025-02-18 00:30:44 +0200

    Update port

--- main.py
+++ main.py
@@ -117,5 +117,6 @@ async def visualize_endpoint(app_id: str):
 
 
 if __name__ == "__main__":
+    import os
     logger.info("Starting API server on http://127.0.0.1:8000")
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=int(os.getenv("PORT", 8000)), reload=True)
